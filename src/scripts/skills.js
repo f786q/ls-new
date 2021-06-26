914c05044fe3be283ae7b5099cc5bd01d3f95fd1
f786q <f786q@ya.ru>
2021-06-26 19:28:32 +0600

    W-2 | add skills component vue

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,32 @@
-console.log('this is the skills module');
+import Vue from 'vue'
+
+const skillsItem = {
+  props: ['skill'],
+  template: '#skills-item',
+  name: 'skills-item',
+  mounted() {
+    const circle = this.$refs["colored-circle"]
+    const dashArray = parseInt(getComputedStyle(circle).getPropertyValue('stroke-dasharray'));
+    const percent = (dashArray / 100) * (100 - this.skill.percent);
+    circle.style.strokeDashoffset = percent; 
+  },
+}
+const skillsRow = {
+  props: ['category'],
+  template: '#skills-row',
+  name: 'skills-row',
+  components: { 'skills-item': skillsItem },
+}
+new Vue({
+  el: '#skills-component',
+  template: '#skills-list',
+  components: { 'skills-row': skillsRow },
+  data() {
+    return {
+      skills: [],
+    }
+  },
+  created() {
+    this.skills = require('/src/data/skills.json')
+  },
+})
